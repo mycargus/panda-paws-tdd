c5d99776feeb9c8a8c155bf83c0da7cb99e4a8b9
Michael Hargiss <mdhargiss@gmail.com>
2016-01-26 05:49:27 -0700

    add method description

--- assignees_list.rb
+++ assignees_list.rb
@@ -1,4 +1,12 @@
 
+# This method returns a sorted English arrangement of the assignees array.
+# Example: Given the assignees array ['Section A', 'Section B'], the method
+#          will return 'Sections A and B'
+# Example: Given the assignees array ['Section A', 'Student 1'], the method
+#          will return 'Section A and Student 1'
+# Example: Given ['Section A', 'Section B', 'Section C'], returns 'Sections A, B, and C'
+# Example: Given ['Section A', 'Section B', 'Section C', 'Student 1', 'Student 4'],
+#          returns 'Sections A, B, and C, and Students 1 and 4'
 def assignees_list(assignees)
   organized_assignees = []
   types = [ 'Section', 'Student', 'Group' ]
